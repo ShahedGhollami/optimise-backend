72a032fc1d4b991888cc32f0fb906de5cbeb4785
ShahedGhollami <shahedghollami@gmail.com>
2026-05-07 12:08:40 +0100

    Add .gitignore file and update project references

--- obj/Debug/net10.0/Optimise.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Optimise.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Optimise.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef27f1909ee9a6b0404b63ce17840bc8bc8f3b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ecdd63dd216a7e3b319c52aa49520e01cd4a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Optimise.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Optimise.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
